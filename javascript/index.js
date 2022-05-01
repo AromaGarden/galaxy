763298ad64d9d93826da19533e28ea1825ba5a24
Wyrd <wyrdparade@gmail.com>
2022-04-30 20:07:43 -0400

    init

---
+++ javascript/index.js
@@ -0,0 +1,14 @@
+
+function space(){
+    console.log({name:'mark',2:[1,2,'joe']})
+
+    var moe = 'stick';
+    
+    
+    let markDate = 'welp';
+    console.log(markDate)
+    
+    const JOE = 'sale';
+    
+}
+space()
